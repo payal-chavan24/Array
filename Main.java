5a90f68de262c43568ff2ee2547cc67a3c3ae615
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-05-14 22:06:34 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package practice_18april;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Student> list = new ArrayList<>();
+		list.add(new Student(1, "Payal", 89));
+		list.add(new Student(2, "Shruti", 86));
+		list.add(new Student(3, "Sonali", 49));
+		list.add(new Student(4, "megha", 99));
+		list.add(new Student(5, "Priya", 29));
+		
+
+	}
+}
